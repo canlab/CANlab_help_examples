cad6ea2a0f4bf2e8d6c4bdabfa51c839fc327eeb
torwager <torwager@gmail.com>
2016-08-03 12:41:23 -0600

    Tor updated initial files

--- example_help_files/load_a_sample_dataset.m
+++ example_help_files/load_a_sample_dataset.m
@@ -3,12 +3,22 @@
 % an fmri_data object.
 
 %% General instructions
-
+%
 % Before you start, the CANlab_Core_Tools must be added to your path with
 % subfolders. Otherwise, you will get errors.
 %
-% The sample datasets are in the Sample_datasets folder.
-% This example will use emotion regulation data in the folder Wager_et_al_2008_Neuron_EmotionReg
+% Sample datasets are in the "Sample_datasets" folder in CANlab_Core_Tools.
+%
+% This example will use emotion regulation data in the folder: 
+% "Wager_et_al_2008_Neuron_EmotionReg"
+% The dataset is a series of contrast images from N = 30 participants.
+% Each image is a contrast image for [reappraise neg vs. look neg]
+% 
+% These data were published in:
+% Wager, T. D., Davidson, M. L., Hughes, B. L., Lindquist, M. A., 
+% Ochsner, K. N.. (2008). Prefrontal-subcortical pathways mediating 
+% successful emotion regulation. Neuron, 59, 1037-50.
+%
 %
 % Here are a couple of helpful functions we will use for display:
 % (you can ignore these.)
@@ -16,6 +26,23 @@ dashes = '----------------------------------------------';
 printhdr = @(str) fprintf('%s\n%s\n%s\n', dashes, str, dashes);
 
 %% Section 1: The quick and easy way to load a pre-specified dataset
+%
+% The function load_image_set has a number of pre-defined image sets that
+% you can load with one simple command.  This is the easiest way to load
+% sample data.  The images must be on your Matlab path for this to work.
+
+[data_obj, subject_names, image_names] = load_image_set('emotionreg');
+
+% data_obj is an fmri_data object containing all 30 images.
+% subject_names is a list of short names for each image.
+% image_names is a list of the full image names with their path.
+
+%% Section 2: Manual load.  Use filenames to find file names 
+%
+% First, we need to list the file names in a string matrix.
+% Then, we can load them into an fmri_data object
+% We will use the filenames function to get the names, and the fmri_data
+% object constructor function to load the data.
 
 % First, check whether images are on your path:
 % We will search for one image and save the path name.
@@ -25,17 +52,15 @@ myfile = which('con_00810001.img');
 mydir = fileparts(myfile);
 if isempty(mydir), disp('Uh-oh! I can''t find the data.'), else disp('Data found.'), end
 
-
-%% Section 2: Use filenames to find file names 
-
-% First, we need to list the file names in a string matrix.
-% Then, we can load them into an fmri_data object
+% Now we can list all the file names.
 
 printhdr('Find files and get their names:')
 image_names = filenames(fullfile(mydir, 'con_008100*img'), 'absolute');
+disp('Done.');
 
 % Now load them into an fmri_data object.
 
+printhdr('Loading the image data into the object:')
 data_obj = fmri_data(image_names);
 
 % This is the gateway to doing many other things, which are explained in
@@ -44,7 +69,7 @@ data_obj = fmri_data(image_names);
 % the data.
 
 %% Section 3: Plot the data we just loaded
-
+%
 % Operations that we can perform on fmri_data objects are called methods.
 % You can see a list of methods by typing methods(data_obj).
 % Here, we'll call the plot method to visualize the data.
--- publish_help_files/CANlab_help_examples_TEMPLATE.m
+++ publish_help_files/CANlab_help_examples_TEMPLATE.m
@@ -2,13 +2,36 @@
 % One-line description of what this help file shows.
 
 %% General instructions
-
-% Code blocks with %% will appear as sections in the table of contents.
-% Comments fowing % will be printed as text.
+%
+% Instructions for creating a file:
+% -----------------------------------------------------------------------
+%
+% - Try to give a brief, but complete, description
+% - Code blocks with %% will appear as sections in the table of contents.
+% - Comments following % will be printed as text.
+% - Comment lines *immediately below a %% code block will be printed as
+% regular text.
+%
+% Instructions to include in help files as appropriate:
+% -----------------------------------------------------------------------
+%
+% Before you start, the CANlab_Core_Tools must be added to your path with
+% subfolders. Otherwise, you will get errors.
+%
+% Sample datasets are in the "Sample_datasets" folder in CANlab_Core_Tools.
+%
+% This example will use emotion regulation data in the folder: 
+% "Wager_et_al_2008_Neuron_EmotionReg"
+% The dataset is a series of contrast images from N = 30 participants.
+% Each image is a contrast image for [reappraise neg vs. look neg]
+% 
+% These data were published in:
+% Wager, T. D., Davidson, M. L., Hughes, B. L., Lindquist, M. A., 
+% Ochsner, K. N.. (2008). Prefrontal-subcortical pathways mediating 
+% successful emotion regulation. Neuron, 59, 1037-50.
 
 %% Section 1: Creating figures in your code
-% --------------------------------------------------------------
-
+%
 % use snapnow to take a snapshot of a figure in the HTML file.
 % Example:
 
@@ -19,10 +42,9 @@ snapnow
 
 
 %% Section 2: Helper functions and printing text output
-% --------------------------------------------------------------
-
-% Let's create a standard set of helper functions to increase readability
-% Display helper functions: Called by later scripts
+%
+% Let's create a standard set of helper functions to increase readability.
+% You can include these in your help scripts.
 
 dashes = '----------------------------------------------';
 printstr = @(dashes) disp(dashes);
