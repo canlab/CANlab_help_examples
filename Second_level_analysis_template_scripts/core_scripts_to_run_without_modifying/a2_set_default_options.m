85e3d91b1165f67b3045ec19cddba942b7468542
Tor Wager <tor.wager@colorado.edu>
2017-06-16 11:08:04 -0600

    resolve a2 default option conflict

--- Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/a2_set_default_options.m
+++ Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/a2_set_default_options.m
@@ -6,14 +6,15 @@
 
 % prep_2_load_image_data_and_save options  (also prep_3_calc_univariate_contrast_maps_and_save)
 % --------------------------------------------------------------------
-dofullplot = false;         % default true  Can set to false to save time
-omit_histograms = true;     % default false Histograms not useful for large samples 
-dozipimages = false;        % default true  Set to false to avoid load on data upload/download when re-running often
+dofullplot = true;         % default true  Can set to false to save time
+omit_histograms = false;     % default false Histograms not useful for large samples 
+dozipimages = true;        % default true  Set to false to avoid load on data upload/download when re-running often
 
 
 % c2_SVM_contrasts options
 % --------------------------------------------------------------------
 dosavesvmstats = true;      % default true      Save statistics and weight map objects for SVM contrasts
-dobootstrap = true;         % default false     Takes a lot of time
-boot_n = 100;               % default number of bootstrap samples. Very slow. Recommend 5,000 for final published analysis
+dobootstrap = false;         % default false     Takes a lot of time
+boot_n = 1000;              % default number of bootstrap samples. Very slow. Recommend 5,000 for final published analysis
+
 
