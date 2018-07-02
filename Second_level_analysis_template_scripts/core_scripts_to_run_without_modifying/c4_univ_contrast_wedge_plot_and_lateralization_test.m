b246bc8cf1a658766f752ab4fc88076a9f2df745
Tor Wager <tor.wager@colorado.edu>
2018-07-01 21:55:07 -0600

    very minor update to lat test

--- Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/c4_univ_contrast_wedge_plot_and_lateralization_test.m
+++ Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/c4_univ_contrast_wedge_plot_and_lateralization_test.m
@@ -1,4 +1,17 @@
-
+% This script extracts averages for 17 networks based on rsfMRI connectivity, 
+% based on a paper (Schaefer et al. 2018) and Github repo from Thomas Yeo?s lab.  
+% It tests the average activity and, because each network can be divided into a 
+% left and right hemisphere part, it tests for significant lateralization as well.  
+%
+% This script in the ?Canlab_help_examples? Github repo wraps an
+% image_vector method that does the core test, and is compatible with
+% Matlab's publish command to produce HTML reports. 
+%
+% The core method to run on any fmri_data object is : 
+% image_vector/ttest_table_and_lateralization_test
+%
+% You?ll need the Neuroimaging_Pattern_Masks repository from the Canlab Github repository set as well.
+% https://github.com/canlab/Neuroimaging_Pattern_Masks
 
 %% Wedge plot, table, and lateralization for 17 Yeo et al. networks
 % --------------------------------------------------------------------
@@ -35,14 +48,14 @@ for c = 1:kc
         
     end
     
-    drawnow, snapnow
+    % drawnow, snapnow
     
     % Save figure
     ff = findobj('Tag', 'wedge overall importance');
-    figure(ff)
+    figure(ff);
     set(ff, 'Tag', figtitle);
     
-    plugin_save_figure;
+    plugin_save_figure;  % and drawnow, snapnow
     
 end
 
