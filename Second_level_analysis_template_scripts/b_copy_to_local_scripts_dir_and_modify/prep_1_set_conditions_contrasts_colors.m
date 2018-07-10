dda74f5c45db9990d770963fa4596bb1419d01cd
Tor Wager <tor.wager@colorado.edu>
2018-07-10 16:48:22 -0600

    update prep_1 help.  update unzip operation.

--- Second_level_analysis_template_scripts/b_copy_to_local_scripts_dir_and_modify/prep_1_set_conditions_contrasts_colors.m
+++ Second_level_analysis_template_scripts/b_copy_to_local_scripts_dir_and_modify/prep_1_set_conditions_contrasts_colors.m
@@ -12,13 +12,82 @@ fprintf('Image data should be in /data folder\n');
 
 DAT = struct();
 
-% Names of subfolders in /data
-DAT.subfolders = {'Pain_copes' 'Nausea_cope4m1' 'Itch_copes_combo'};
-
 % Names of conditions
+% A "condition" usually corresponds to a regressor (beta/COPE image) in a first-level 
+% (single-subject) analysis. You will usually have one image per condition per subject.
+% You will have the opporunity below to specify contrasts across these
+% conditions, which are estimated as part of the prep_ scripts.  
+% For example, if you have a task with three types of stimuli, "pain"
+% "nausea" and "itch", there would be 3 conditions. 
+%
+% Within-subjects design:  If this is a within-person design
+% (each person experiences all three conditions), you would have 3 images
+% per subject in your "data" subfolder for this study. When the images are
+% loaded (later, by the prep_2_... script) the subject images should be in
+% the same order for each of the three conditions. 
+%
+% Between-subjects design:  If this is a between-person design, you would have 3 groups 
+% of subjects with one image each. You may have different numbers of
+% subjects in each condition.
+% 
+% Designs with multiple events per task condition: Sometimes, you may have
+% multiple images corresponding to the same task/trial type. For example,
+% you might have 4 levels of "pain", or "early pain" and "late pain".  It
+% is recommended to enter each level as a separate "condition". You can
+% later create contrasts, e.g., linear contrast across 4 pain levels, or
+% "early vs. late". 
+
+% Enter a cell array { } with one cell per condition.  Each cell should
+% contain a string specifying the condition name to be used in plots and
+% tables. This does not have to correspond to an image/directory name.
+
 DAT.conditions = {'Pain' 'Nausea' 'Itch'};
 
 DAT.conditions = format_strings_for_legend(DAT.conditions);
+
+% SPECIFYING IMAGE FILE LOCATIONS FOR EACH CONDITION
+% ------------------------------------------------------------------------
+% The next lines of code specify how to locate the image files associated
+% with each condition.  The image load script (i.e., prep_2...m) will attempt to use
+% the file system (mac osx/windows/linux) to list the files and store them
+% in a cell array of strings, with each string containing the full path name 
+% for a valid image.  Images will be listed in the order in which the file
+% system lists them.  
+% For each condition, the prep_2...m script will construct a string containing
+% wildcards (*, ?, [1-9], etc.) that can help you flexibly specify the location of 
+% image.  The string is fullfile(datadir, DAT.subfolders{i}, DAT.functional_wildcard{i}),
+% where i is the condition number and datadir is the path to the main
+% "data" subfolder.
+%
+% EXAMPLES:
+% Here is an example.  Say you have a subfolder in [basedir]/data 
+% called 'SAC_NRFX/contrast_2', which contains one image per person, 
+% named con_2_subj01.img, con_2_subj02.img, etc.  
+% You could enter 'SAC_NRFX/contrast_2' in the .subfolders field below, 
+% and 'con*img' in the functional_wildcard field below, and the result
+% would be that we'd list files in
+% [basedir]/data/SAC_NRFX/contrast_2/con*img.
+%
+% In a second example, say you have a subfolder in [basedir]/data for each
+% subject. Inside that folder are images called "pain.nii" and "itch.nii"
+% You could enter '*' in the .subfolders field below, 
+% and 'pain.nii' in the functional_wildcard field for condition 1, and "itch.nii"
+% for condition 2.  The result
+% would be that we'd list files in
+% [basedir]/data/*/pain.nii for condition 1, and [basedir]/data/*/itch.nii
+
+% Names of subfolders in [basedir]/data
+% Enter a cell array { } with one cell per condition.  Each cell should
+% contain a string specifying the subfolder to look in for images of this
+% condition. 
+% If you do not have subfolders, it is OK to leave this empty, i.e., DAT.subfolders = {};
+
+DAT.subfolders = {'Pain_copes' 'Nausea_cope4m1' 'Itch_copes_combo'};
+
+% Names of wildcard (expression with *, [1-9], 
+% Enter a cell array { } with one cell per condition.  Each cell should
+% contain a string specifying the subfolder to look in for images of this
+% condition. 
 
 DAT.structural_wildcard = {};
 DAT.functional_wildcard = {'cope3*nii' 'cope4*nii' 'cope1*nii'};
@@ -49,15 +118,27 @@ DAT.functional_wildcard = {'cope3*nii' 'cope4*nii' 'cope1*nii'};
 % subjects are nested within conditions (i.e., between-subject design).
 % These will be used in c2c_SVM_between_condition_contrasts.
 
-% Vectors across conditions
+% WITHIN-PERSON CONTRASTS: Vectors across conditions
+% There should be one column in DAT.contrasts per condition, and one row
+% per contrast. Enter contrasts for within-person comparisons only (matched
+% sets of images, where the ith image is from the ith subject for all
+% conditions).
+
 DAT.contrasts = [1 0 0; 0 1 0; 0 0 1];
     
+% Descriptive names for contrasts to be used in plots and tables. Avoid
+% special characters.
 DAT.contrastnames = {'Pain' 'Nausea' 'Itch'};
 
 DAT.contrastnames = format_strings_for_legend(DAT.contrastnames);
 
 % Set Colors
 % ------------------------------------------------------------------------
+
+% DAT.colors should be a cell array { } with one 3-element rgb vector
+% (e.g., [1 0 0] for red) per condition.
+
+% If you do not edit the code below it is ok; sensible default choices are already coded up. 
 
 % There are several options for defining colors for conditions and
 % contrasts, or enter your own in a cell array of length(conditions) for
@@ -73,6 +154,10 @@ DAT.contrastnames = format_strings_for_legend(DAT.contrastnames);
 mycolors = colorcube_colors(length(DAT.conditions) + size(DAT.contrasts, 1));
 
 DAT.colors = mycolors(1:length(DAT.conditions));
+
+% DAT.contrastcolors should be a cell array { } with one 3-element rgb vector
+% (e.g., [1 0 0] for red) per contrast.
+
 DAT.contrastcolors = mycolors(length(DAT.conditions) + 1:length(mycolors));
 
 
