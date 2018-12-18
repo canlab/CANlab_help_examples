3937a1997ee9836b46eef0dc1cf47bbc474a0673
Tor Wager <tor.wager@colorado.edu>
2018-12-17 18:31:01 -0700

    Canlab methods diagram added

---
+++ diagrams/canlab_methods_coggle/CANlab Methods.mm
@@ -0,0 +1,179 @@
+
+<map version="0.9.0">
+    <node TEXT="[CANlab Methods](https://canlab.github.com)" FOLDED="false" POSITION="right" ID="5c078ccc16774c343aa90a81" X_COGGLE_POSX="0" X_COGGLE_POSY="0">
+        <edge COLOR="#efa670"/>
+        <font NAME="Helvetica" SIZE="16"/>
+        <node TEXT="meta-analysis" FOLDED="false" POSITION="right" ID="765b0f20616acc1e81cb783910e0ce38">
+            <edge COLOR="#ebd95f"/>
+            <font NAME="Helvetica" SIZE="20"/>
+            <node TEXT="[Coordinate-based: Multilevel Kernel Density Analysis (MKDA)](https://github.com/canlab/Canlab_MKDA_MetaAnalysis)" FOLDED="false" POSITION="left" ID="839e6086d0467f038a6e25deb196d5ea">
+                <edge COLOR="#e8d358"/>
+                <font NAME="Helvetica" SIZE="15"/>
+                <node TEXT="Multivariate: Correspondence, Clustering" FOLDED="false" POSITION="right" ID="eef5b5b912bed89282c6c3d8995e3d3e">
+                    <edge COLOR="#e8d364"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+            </node>
+            <node TEXT="Image-based: Generalized Inverse Variance, meta-regression" FOLDED="false" POSITION="right" ID="7bec4fc0963fb689a80752041c1c55da">
+                <edge COLOR="#ecd758"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+        </node>
+        <node TEXT="process models" FOLDED="false" POSITION="right" ID="6ddf20e7ce356021d12d05eb711fbbc3">
+            <edge COLOR="#69b500"/>
+            <font NAME="Helvetica" SIZE="20"/>
+            <node TEXT="Dynamic Causal Modeling" FOLDED="false" POSITION="right" ID="7f4990adcd67635656b9cbc21aa79625">
+                <edge COLOR="#6bb201"/>
+                <font NAME="Helvetica" SIZE="16"/>
+            </node>
+            <node TEXT="Reinforcement learning" FOLDED="false" POSITION="right" ID="f7fa69687bdacc276dc9e049aadfec15">
+                <edge COLOR="#66ab00"/>
+                <font NAME="Helvetica" SIZE="16"/>
+            </node>
+            <node TEXT="hierarchical Bayes" FOLDED="false" POSITION="right" ID="89f3e07a94a141b56be0544ef9b32259">
+                <edge COLOR="#67ad00"/>
+                <font NAME="Helvetica" SIZE="16"/>
+            </node>
+        </node>
+        <node TEXT="Anatomical localization and visualization" FOLDED="false" POSITION="left" ID="36e60195a4fbc40aa9604779731972ad">
+            <edge COLOR="#3e8975"/>
+            <font NAME="Helvetica" SIZE="20"/>
+            <node TEXT="Batch results and reports" FOLDED="false" POSITION="left" ID="b2668fbbbb88e319f25b24cf147bf8af">
+                <edge COLOR="#3c8772"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="Network decomposition, polar and wedge plots" FOLDED="false" POSITION="left" ID="18904435f2f9251c034a1732e588de65">
+                <edge COLOR="#418c77"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="Interactive montages, surfaces" FOLDED="false" POSITION="left" ID="befc1bc0e4dc9cbae9e175cf1536451c">
+                <edge COLOR="#38836e"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+        </node>
+        <node TEXT="point-process and change point models: HEWMA" FOLDED="false" POSITION="right" ID="4fe9da9842631b19279ede571cdad1d4">
+            <edge COLOR="#0a660d"/>
+            <font NAME="Helvetica" SIZE="15"/>
+        </node>
+        <node TEXT="[connectivity, dynamic connectivity](https://github.com/canlab/CanlabCore)" FOLDED="false" POSITION="right" ID="10231585bc1eb8605ef3b0335618cf81">
+            <edge COLOR="#0da7d3"/>
+            <font NAME="Helvetica" SIZE="17"/>
+            <node TEXT="Stochastic blockmodels" FOLDED="false" POSITION="right" ID="9baad1f560edfe8489499227604aed01">
+                <edge COLOR="#0da7d3"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="Graph-theoretic models" FOLDED="false" POSITION="right" ID="86bb00eb02cfa9ce9c5108f666b1c063">
+                <edge COLOR="#0ca1c8"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+        </node>
+        <node TEXT="regression and classification" FOLDED="false" POSITION="left" ID="988612ae6c2c8874453f0c7bd66e8084">
+            <edge COLOR="#988ee3"/>
+            <font NAME="Helvetica" SIZE="20"/>
+            <node TEXT="[Spatial statistics: MANOVA, confidence volumes, Bayesian](https://github.com/canlab/CanlabCore)" FOLDED="false" POSITION="left" ID="f94c46256089c18fdced459048c7164c">
+                <edge COLOR="#9e96e7"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="[Multilevel mediation](https://github.com/canlab/RobustToolbox)" FOLDED="false" POSITION="right" ID="8163e15872abfa7eb38b1218f4905767">
+                <edge COLOR="#9e96e7"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="[Multivariate regression - LASSO-PCR](https://github.com/canlab/CanlabCore)" FOLDED="false" POSITION="right" ID="713e5b7d0db45b855acf309b207fc969">
+                <edge COLOR="#8e84e1"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="[Robust regression](https://github.com/canlab/RobustToolbox)" FOLDED="false" POSITION="left" ID="a69622bf311ee2ba1485fb3c20cd2782">
+                <edge COLOR="#9086e3"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="Representational Similarity Analysis" FOLDED="false" POSITION="right" ID="be057a3a172fd6d40e32208757ff7e1d">
+                <edge COLOR="#988fe4"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+        </node>
+    </node>
+    <x-coggle-rootnode TEXT="[*Kober et al. 2008*](https://example.com)" FOLDED="false" POSITION="right" ID="cbd45c233d76205f6a872c77c5fa8359" X_COGGLE_POSX="6" X_COGGLE_POSY="-418">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Pauli et al. 2016*](https://example.com)" FOLDED="false" POSITION="right" ID="610ecb940e7f76d2233616600d4b20fd" X_COGGLE_POSX="5" X_COGGLE_POSY="-321">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Wager et al. 2009*](https://example.com)" FOLDED="false" POSITION="right" ID="4eea48202957b1052a59f4f3749063bd" X_COGGLE_POSX="-296" X_COGGLE_POSY="-228">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Zunhammer et al. 2018*](https://example.com)" FOLDED="false" POSITION="right" ID="9ac188ca33ecb7a77cf34b6a9ea88073" X_COGGLE_POSX="248" X_COGGLE_POSY="-209">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Roy et al. 2014*](https://example.com)" FOLDED="false" POSITION="right" ID="e9ae0a0a861473f26ff3e83bfdd7131a" X_COGGLE_POSX="482" X_COGGLE_POSY="-150">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Eisenbarth et al. 2016*](https://example.com)" FOLDED="false" POSITION="right" ID="04d9fe719b9f31231d9ebd7b857a395c" X_COGGLE_POSX="-547" X_COGGLE_POSY="20">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Jepma et al. 2018*](https://example.com)" FOLDED="false" POSITION="right" ID="b3b579618b584ea5354a9635ad4a9f00" X_COGGLE_POSX="480" X_COGGLE_POSY="36">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Lindquist et al. 2006*](https://example.com)" FOLDED="false" POSITION="right" ID="5dabc690c7e3d4ed9eaf6d2b818d7e8f" X_COGGLE_POSX="327" X_COGGLE_POSY="131">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Robinson et al. 2015*](https://example.com)" FOLDED="false" POSITION="right" ID="263f5a4c926757e265c148ea82b1f7f9" X_COGGLE_POSX="466" X_COGGLE_POSY="199">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Wager et al. 2002*](https://example.com)" FOLDED="false" POSITION="right" ID="406d9d40bafe00fdd39c7bdf6b378e4f" X_COGGLE_POSX="-443" X_COGGLE_POSY="257">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Reddan et al. 2018*](https://example.com)" FOLDED="false" POSITION="right" ID="ff2f2de7e91385b12ec8033c3d08c796" X_COGGLE_POSX="450" X_COGGLE_POSY="271">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Kang et al. 2011*](https://example.com)" FOLDED="false" POSITION="right" ID="3145b86f478edb9572c4fc099f0ff596" X_COGGLE_POSX="-390" X_COGGLE_POSY="285">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Woo et al. 2015*](https://example.com)" FOLDED="false" POSITION="right" ID="d47d73429af6343bfa6f4388a049fd6b" X_COGGLE_POSX="286" X_COGGLE_POSY="293">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Wager et al. 2008*](https://example.com)" FOLDED="false" POSITION="right" ID="b6bf05d8b482af35bd2e713267743b7e" X_COGGLE_POSX="241" X_COGGLE_POSY="363">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Koban et al. 2017*](https://example.com)" FOLDED="false" POSITION="right" ID="0f55a8efe1eb6f614db9733259906f97" X_COGGLE_POSX="303" X_COGGLE_POSY="387">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Wager et al. 2011*](https://example.com)" FOLDED="false" POSITION="right" ID="a74b56d6328c33d6f8cbd96d71094a8f" X_COGGLE_POSX="7" X_COGGLE_POSY="446">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Wager et al. 2005*](https://example.com)" FOLDED="false" POSITION="right" ID="3c50b81390097372567631fa2483edfd" X_COGGLE_POSX="-292" X_COGGLE_POSY="464">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="[*Kragel et al. 2018*](https://example.com)" FOLDED="false" POSITION="right" ID="3c7ebcb708840cd7b6c1971df7641cb7" X_COGGLE_POSX="132" X_COGGLE_POSY="560">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="14"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="" FOLDED="false" POSITION="right" ID="f8a59e9d398b3ad675a36106a76e59f2" X_COGGLE_POSX="-303" X_COGGLE_POSY="592">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="17"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="" FOLDED="false" POSITION="right" ID="67623d721123b5ffdd74999b65f21e23" X_COGGLE_POSX="-71" X_COGGLE_POSY="654">
+        <edge COLOR="#a65427"/>
+        <font NAME="Helvetica" SIZE="50"/>
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="" FOLDED="false" POSITION="right" ID="67623d721123b5ffdd74999b65f21e23" X_COGGLE_POSX="-71" X_COGGLE_POSY="654">
+        <edge COLOR="#a65427"/>
+        <font NAME="Helvetica" SIZE="50"/>
+    </x-coggle-rootnode>
+</map>
